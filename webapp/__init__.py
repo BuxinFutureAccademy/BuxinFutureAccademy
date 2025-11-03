7c4759032dee0a4b3471beec5e2c06b65b63a03f
Abdoukadir Jabbi <buxinjabbi@gmail.com>
2025-11-03 17:56:22 +0530

    update: applied project changes

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -52,7 +52,7 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'auth.login'
 
     from .models.users import User
 
